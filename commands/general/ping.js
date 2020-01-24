f001f39ce0af2fd88e800bbc8d4c86fa3c40f80a
Boss <austajm4185@gmail.com>
2020-01-24 16:52:27 -0500

    attempted to fix help command

--- commands/general/ping.js
+++ commands/general/ping.js
@@ -2,6 +2,7 @@ module.exports = {
     name: 'ping', 
     category: 'info',
     description: 'Pings the bot.',
+    alaises: ['pong'], 
     run: async (client, message, args) => { 
         let time = Date.now(); 
         let msg = await message.channel.send('Ping!'); 
