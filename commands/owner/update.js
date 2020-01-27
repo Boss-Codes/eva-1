0b9a939c49fc7638f6835d577e0665cac9694831
Boss <austajm4185@gmail.com>
2020-01-26 20:31:34 -0500

    daddy && git push

---
+++ commands/owner/update.js
@@ -0,0 +1,20 @@
+const {  exec } = require('child_process'); 
+module.exports = { 
+    name: 'update', 
+    run: async (client, message, args) => { 
+
+        if(message.author.id !== '344954369285947392') return; 
+         
+        let commitm = args.join(' ')
+
+        exec(`git add . && git commit -m "${commitm.toString()} && git push`), (error, stdout) => {
+            const outputType = error || stdout;
+            let output = outputType;
+            if (typeof outputType === 'object') {
+                output = inspect(outputType);
+            }
+            output = (output.length > 1980 ? output.substr(0, 1977) + '...' : output);
+            return message.channel.send('`\``' + output + '`\``');
+        };
+    }
+    }
